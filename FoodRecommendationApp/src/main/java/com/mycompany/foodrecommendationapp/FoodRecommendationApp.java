28de51eae137c3e17c6aed5c205ca86f4df57c5a
NiegelF <107429076+NiegelF@users.noreply.github.com>
2023-09-12 08:16:02 -0400

    Add files via upload

--- FoodRecommendationApp/src/main/java/com/mycompany/foodrecommendationapp/FoodRecommendationApp.java
+++ FoodRecommendationApp/src/main/java/com/mycompany/foodrecommendationapp/FoodRecommendationApp.java
@@ -18,21 +18,24 @@ import java.awt.event.MouseEvent;
 public class FoodRecommendationApp extends JFrame {
     private final JTextField budgetField;
     private final JTextField calorieField;
+    private final JTextField heightField;
+    private final JTextField weightField;
+    private final JComboBox<String> genderComboBox;
     private JTextArea recommendationArea;
     private JButton recommendButton;
 
-    // Sample food data (name, price, calories)
+    // Sample food data (name, price, calories, where to find, macros)
     private String[][] foodData = {
-            {"Rice", "2.50", "200"},
-            {"Pasta", "4.50", "250"},
-            {"Oatmeal", "2.00", "150"},
-            {"Peanut Butter", "3.50", "180"},
-            {"Potatoes", "2.00", "160"},
-            {"Beans", "1.50", "200"},
-            {"Eggs", "3.00", "150"},
-            {"Bananas", "0.50", "105"},
-            {"Bread", "2.00", "200"},
-            {"Pasta Sauce", "1.50", "70"},
+            {"Rice", "2.50", "200", "Grocery Store A", "Carbs: 45g, Protein: 5g, Fat: 0.5g"},
+            {"Pasta", "4.50", "250", "Grocery Store B", "Carbs: 50g, Protein: 10g, Fat: 2g"},
+            {"Oatmeal", "2.00", "150", "Grocery Store A", "Carbs: 30g, Protein: 5g, Fat: 2.5g"},
+            {"Peanut Butter", "3.50", "180", "Grocery Store B", "Carbs: 7g, Protein: 8g, Fat: 14g"},
+            {"Potatoes", "2.00", "160", "Local Market", "Carbs: 37g, Protein: 2g, Fat: 0.2g"},
+            {"Beans", "1.50", "200", "Local Market", "Carbs: 35g, Protein: 7g, Fat: 0.5g"},
+            {"Eggs", "3.00", "150", "Grocery Store A", "Carbs: 1g, Protein: 13g, Fat: 10g"},
+            {"Bananas", "0.50", "105", "Local Market", "Carbs: 27g, Protein: 1g, Fat: 0.3g"},
+            {"Bread", "2.00", "200", "Grocery Store B", "Carbs: 40g, Protein: 10g, Fat: 1g"},
+            {"Pasta Sauce", "1.50", "70", "Grocery Store B", "Carbs: 14g, Protein: 2g, Fat: 0.5g"},
             // Add more budget-friendly, high-calorie foods here...
     };
 
@@ -51,6 +54,13 @@ public class FoodRecommendationApp extends JFrame {
         budgetField = new JTextField(10);
         JLabel calorieLabel = new JLabel("Calorie Needs:");
         calorieField = new JTextField(10);
+        JLabel heightLabel = new JLabel("Height (cm):");
+        heightField = new JTextField(10);
+        JLabel weightLabel = new JLabel("Weight (kg):");
+        weightField = new JTextField(10);
+        JLabel genderLabel = new JLabel("Gender:");
+        String[] genderOptions = {"Male", "Female"};
+        genderComboBox = new JComboBox<>(genderOptions);
         recommendButton = new JButton("Recommend");
 
         inputPanel.add(budgetLabel, constraints);
@@ -62,6 +72,21 @@ public class FoodRecommendationApp extends JFrame {
         constraints.gridx = 1;
         inputPanel.add(calorieField, constraints);
         constraints.gridy = 2;
+        constraints.gridx = 0;
+        inputPanel.add(heightLabel, constraints);
+        constraints.gridx = 1;
+        inputPanel.add(heightField, constraints);
+        constraints.gridy = 3;
+        constraints.gridx = 0;
+        inputPanel.add(weightLabel, constraints);
+        constraints.gridx = 1;
+        inputPanel.add(weightField, constraints);
+        constraints.gridy = 4;
+        constraints.gridx = 0;
+        inputPanel.add(genderLabel, constraints);
+        constraints.gridx = 1;
+        inputPanel.add(genderComboBox, constraints);
+        constraints.gridy = 5;
         constraints.gridx = 0;
         constraints.gridwidth = 2;
         inputPanel.add(recommendButton, constraints);
@@ -103,6 +128,17 @@ public class FoodRecommendationApp extends JFrame {
     private void recommendFood() {
         double budget = Double.parseDouble(budgetField.getText());
         int calorieNeeds = Integer.parseInt(calorieField.getText());
+        int height = Integer.parseInt(heightField.getText());
+        double weight = Double.parseDouble(weightField.getText());
+        String gender = (String) genderComboBox.getSelectedItem();
+
+        // Calculate daily calorie needs based on user input (a basic calculation)
+        int dailyCalorieNeeds;
+        if (gender.equals("Male")) {
+            dailyCalorieNeeds = (int) (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * 25));
+        } else {
+            dailyCalorieNeeds = (int) (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * 25));
+        }
 
         StringBuilder recommendations = new StringBuilder("Recommended Foods:\n");
         double totalCost = 0.0;
@@ -112,9 +148,14 @@ public class FoodRecommendationApp extends JFrame {
             String foodName = food[0];
             double foodPrice = Double.parseDouble(food[1]);
             int foodCalories = Integer.parseInt(food[2]);
+            String whereToFind = food[3];
+            String macros = food[4];
 
-            if (totalCost + foodPrice <= budget && foodCalories <= calorieNeeds) {
-                recommendations.append(foodName).append(" ($").append(foodPrice).append(", ").append(foodCalories).append(" calories)\n");
+            double calorieToPriceRatio = foodCalories / foodPrice;
+
+            if (totalCost + foodPrice <= budget && foodCalories <= calorieNeeds && totalCalories + foodCalories <= dailyCalorieNeeds) {
+                recommendations.append(foodName).append(" ($").append(foodPrice).append(", ").append(foodCalories).append(" calories, ").append(macros).append(")\n");
+                recommendations.append("Where to Find: ").append(whereToFind).append("\n\n");
                 totalCost += foodPrice;
                 totalCalories += foodCalories;
             }
@@ -122,6 +163,7 @@ public class FoodRecommendationApp extends JFrame {
 
         recommendations.append("\nTotal Cost: $").append(totalCost);
         recommendations.append("\nTotal Calories: ").append(totalCalories);
+        recommendations.append("\nDaily Calorie Needs: ").append(dailyCalorieNeeds);
         recommendationArea.setText(recommendations.toString());
     }
 
@@ -135,4 +177,5 @@ public class FoodRecommendationApp extends JFrame {
         });
     }
 }
+
 
